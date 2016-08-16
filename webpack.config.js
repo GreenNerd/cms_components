509772fadfd599f5e6226c0e8b3352dc407c6871
leecason <316115413@qq.com>
2016-08-16 17:27:25 +0800

    add images

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,20 @@
 var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
 
 module.exports = {
     entry: "./entry.js",
     output: {
         path: __dirname,
         filename: "bundle.js"
+        publicPath: "/assets/"
     },
     module: {
         loaders: [
-            { test: /\.css$/, loader: "style!css" },
-            { test: /\.scss$/, loader: "style!css!sass"},
+            { test: /\.scss$/, loader: "style!css!postcss-loader!sass"},
             { test: /\.(png|jpg)$/, loader: "url-loader?limit=8192"}
         ]
     },
+    postcss: [ autoprefixer({ browsers: ['Chrome > 1','Firefox > 15','iOS > 5','Android > 2.3'] }) ],
     resolve:{
 
     }
