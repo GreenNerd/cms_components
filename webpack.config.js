ce6243225e7975604d24de72a8cd92945f80ebde
leecason <316115413@qq.com>
2016-08-17 10:30:30 +0800

    indent tab->space

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,17 @@ var autoprefixer = require('autoprefixer');
 module.exports = {
     entry: "./entry.js",
     output: {
-        path: __dirname,
-        filename: "bundle.js"
-        publicPath: "/assets/"
+      path: __dirname,
+      filename: "bundle.js"
+      publicPath: "/assets/"
     },
     module: {
-        loaders: [
-            { test: /\.scss$/, loader: "style!css!postcss-loader!sass"},
-            { test: /\.(png|jpg)$/, loader: "url-loader?limit=8192"}
-        ]
+      loaders: [
+        { test: /\.scss$/, loader: "style!css!postcss-loader!sass"},
+        { test: /\.(png|jpg)$/, loader: "url-loader?limit=8192"}
+      ]
     },
     postcss: [ autoprefixer({ browsers: ['Chrome > 1','Firefox > 15','iOS > 5','Android > 2.3'] }) ],
     resolve:{
-
     }
 };
