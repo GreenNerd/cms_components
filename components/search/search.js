3346ed5aff356a569e718758d4cf329fa34ef647
leecason <316115413@qq.com>
2016-11-23 11:01:07 +0800

    Update search.

--- components/search/search.js
+++ components/search/search.js
@@ -1,20 +1,30 @@
-function Search(searchId,searchSty){
-	const container = document.getElementById(searchId)
-	const searchStyle = searchSty
+(function() {
+  function SearchBox() {
+    const allSearch = document.querySelectorAll("[data-comp='SearchBox']");
 
-	const input = container.getElementsByClassName('search-input');
-	input[0].onfocus = function(){
-		container.classList.add(searchStyle);
-		input[0].style.backgroundImage = 'none';
-	}
-	input[0].onblur = function(){
-		if (input[0].value != '') {
-			input[0].style.backgroundImage = 'none';
-		}else{
-			container.classList.remove(searchStyle);
-			input[0].style.backgroundImage = 'url("/assets/library/search.png")';
-		}
-	}
-}
+    for (var i = allSearch.length - 1; i >= 0; i--) {
+      new Search(allSearch[i]);
+    }
+  }
 
-window.Search = Search;
+  function Search(element) {
+    const container = element;
+    const searchStyle = element.getAttribute('data-style');
+
+    const input = container.querySelector('.search-input');
+    input.onfocus = function(){
+      container.classList.add(searchStyle);
+      input.style.backgroundImage = 'none';
+    }
+    input.onblur = function(){
+      if (input.value != '') {
+        input.style.backgroundImage = 'none';
+      }else{
+        container.classList.remove(searchStyle);
+        input.style.backgroundImage = 'url("/assets/library/search.png")';
+      }
+    }
+  }
+
+  window.Search = SearchBox;
+}());
